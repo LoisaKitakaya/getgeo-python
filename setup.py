386fd533d03c4c874642c3a4a7632a934bb051c9
Freedom Loisa <kitakayaloisa@gmail.com>
2023-05-23 09:43:06 +0300

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="getgeo-python",
+    version="1.0.0",
+    description="",
+    long_description="",
+    author="",
+    author_email="",
+    maintainer="",
+    maintainer_email="",
+    url="",
+    packages=find_packages(),
+    classifiers=[],
+    requires=[],
+)
